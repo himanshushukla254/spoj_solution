b0eab8ed6104d7ece613cd086fea080aabb5b679
himanshushukla254 <himanshushukla254@gmail.com>
2016-01-20 17:42:23 +0530

    spoj ae00 solution

---
+++ ae00.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,i,ans=0;
+    scanf("%d",&n);
+    for(i=1;i<=sqrt(n);i++)
+    {
+        ans=ans+(n/i-i+1);
+    }
+    printf("%d",ans);
+return 0;
+}
