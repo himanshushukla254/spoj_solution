706672edcea7079af2023c303e6e903d3e1371c0
himanshushukla254 <himanshushukla254@gmail.com>
2016-01-20 16:16:58 +0530

    spoj canton solution

---
+++ test_canton.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+        int i,n,sum=0;
+        scanf("%d",&n);
+        for(i=1;;i++)
+        {
+            sum+=i;
+            if(sum>=n)
+                break;
+        }
+        int temp=n-(sum-i);
+        int total=i+1;
+        if(i%2==0)
+            printf("TERM %d IS %d/%d\n",n,temp,total-temp);
+        else
+            printf("TERM %d IS %d/%d\n",n,total-temp,temp);
+    }
+    return 0;
+}
+
